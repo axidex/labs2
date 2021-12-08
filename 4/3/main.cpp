50b58f449078fc162530d76ea013a10f597e7949
axidex <Stepworm@yandex.ru>
2021-12-09 00:04:56 +0300

    4.3

--- 4/3/main.cpp
+++ 4/3/main.cpp
@@ -34,41 +34,72 @@ ostream& operator<<(ostream& s, Edge& e)
 	return s;
 }
 
-void paintgraph(vector<vector<int> >& matrix, vector<int>& visited, int curr=0)
-{	
-	int color_counter = 1;
-    visited[curr] = color_counter;
+void paintgraph(vector<vector<int> >& matrix, vector <int>& color, int curr=0)
+{
+	
+	set<int> neighbours;
 
-    for(int i = 0; i < matrix.size(); ++i)
-    {
-        if (visited[i] != 0)
-            continue;
-        for (auto el : matrix[i])
-        {
-            if (visited[el] != 0 && matrix[el][curr] != 0)
-                continue;
-            visited[el] = color_counter;
-        }
-        color_counter++;
-    }
+
+	queue<int> q;
+	q.push(curr);
+	while (!q.empty())
+	{
+		curr = q.front();
+		q.pop();
+
+		if (color[curr] != 0)
+			continue;
+		neighbours.clear();
+
+		for (int next = 0; next < matrix.size(); next++)
+			if (matrix[curr][next] == 1 || !color[next])
+			{
+				if(color[next])
+				{
+					neighbours.insert(color[next]);
+				}
+				else
+				{
+				    q.push(next);
+				}
+			}
+
+		
+		int color_counter = 1;
+		auto it = neighbours.begin();
+		do
+		{
+		    it=neighbours.find(color_counter);
+			if(it==neighbours.end())
+			{
+				break;
+			}
+			color_counter++;
+		} while (true);
+
+		
+		color[curr] = color_counter;
+	}
 }
 
 int main() {
 
 	vector<vector<int> > mat =
 	{
-	    { 1, 1, 1, 1, 1, 1 },
-	    { 1, 1, 1, 1, 1, 1 },
-	    { 1, 1, 1, 1, 1, 1 },
-	    { 1, 1, 1, 1, 1, 1 },
-	    { 1, 1, 1, 1, 1, 1 },
-	    { 1, 1, 1, 1, 1, 1 }
+	{ 0, 1, 0, 0, 0, 1 },
+	{ 1, 0, 1, 0, 0, 0 },
+	{ 0, 1, 0, 1, 1, 0 },
+	{ 0, 0, 1, 0, 1, 0 },
+	{ 0, 0, 1, 1, 0, 1 },
+	{ 1, 0, 0, 0, 1, 0 }
 	};
-    vector<int> visited (mat.size(), 0);
 
-	paintgraph(mat, visited, 0);
+	vector<int> color(6, 0);
+	paintgraph(mat, color, 0);
 	std::cout << "\nColor: ";
-	for (int i = 0; i < 6; i++) std::cout << " ";
+	for (int i = 0; i < 6; i++) std::cout << color[i] << " ";
+
+	
 
 	return 0;
 }
